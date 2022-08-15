55386f3d4096846da17eefb5057659d904319729
TaylanPostalci <tay.postal@gmail.com>
2022-08-15 09:17:36 -0400

    objectslab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+ name:"Adam",
+ streetAddress:"55 twilightway"
+};
+function updateEmployeeWithKeyAndValue(employee,streetAddress){
+    const newEmployee ={...employee} ;
+    newEmployee[streetAddress] = '11 Broadway'
+    return newEmployee
+   
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,streetAddress){
+    employee[streetAddress] = '12 Broadway';
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee,name){
+  
+  const tempEmployee ={...employee} ;
+
+  delete tempEmployee.name;
+  return tempEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,name){
+  
+  
+    delete employee.name;
+    return employee
+  }
